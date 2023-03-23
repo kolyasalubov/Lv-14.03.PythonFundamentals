f2afca3f0e907f15a481f26574bfa709ba4a1c42
Alex <alexandersychev2005@gmai.com>
2023-03-23 21:54:59 +0200

    Added 3 Homework

---
+++ osychov/HW3/Task1.py
@@ -0,0 +1,10 @@
+string = "Now is better than never."
+better = string.count("better")
+never = string.count("never")
+is1 = string.count("is")
+print(f"Occurence in better - {better}, never - {never}, is - {is1}")
+
+print(string.upper())
+
+print(string.replace("i", "&"))
+
---
+++ osychov/HW3/Task2.py
@@ -0,0 +1,5 @@
+string = "1256"
+number = list(string)
+print(f"Product first and second number - {int(number[0])*int(number[1])}")
+
+print(string[::-1])
