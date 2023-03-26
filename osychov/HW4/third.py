74bd8570f77a69852e69396231a82f5682f1bf86
Alex <alexandersychev2005@gmai.com>
2023-03-26 14:53:38 +0300

    rewrite hw3 and hw4

--- osychov/HW4/third.py
+++ osychov/HW4/third.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def Factorial(n):
     f=1
     while(n>0):
@@ -10,12 +9,5 @@ def Factorial(n):
 Factorial(5)
 
     
-=======
-n=int(input("Enter number:"))
-f=1
-while(n>0):
-    f=f*n
-    n=n-1
-print("Factorial of the number is: ")
-print(f)
->>>>>>> f88526a4f6154e72dd51a3ffb269edf55f5c8a49
+
+
