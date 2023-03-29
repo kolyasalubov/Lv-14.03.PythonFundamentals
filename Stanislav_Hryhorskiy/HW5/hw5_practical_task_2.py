03a6bde73eb52fd6711f5343b77c40059b8a5b25
Stanislav Hryhorskiy <stasg0905@gmail.com>
2023-03-29 19:58:09 +0300

    Updated file hw5_practical_task_2.py in folder HW5

--- Stanislav_Hryhorskiy/HW5/hw5_practical_task_2.py
+++ Stanislav_Hryhorskiy/HW5/hw5_practical_task_2.py
@@ -13,6 +13,13 @@ for element in range(number):
     print(num_prev, end='; ' if element != number - 1 else '.')
     num_prev, num_next = num_next, num_prev + num_next
 
+# The Binet formula for Fibonacci sequence
+print(f"\nThe first numbers of the Fibonacci sequence in quantity n = {number}:", end=' ')
+fi, psi = 0.5 * (1 + 5 ** 0.5), 0.5 * (1 - 5 ** 0.5)
+for item in range(number):
+    fibo_num = round((fi ** item - psi ** item) / (fi - psi))
+    print(fibo_num, end='; ' if item != number - 1 else '.')
+
 # Example of program operation
 # Enter the number of Fibonacci sequence to be displayed: 8
 # The first numbers of the Fibonacci sequence in quantity n = 8: 0; 1; 1; 2; 3; 5; 8; 13.
