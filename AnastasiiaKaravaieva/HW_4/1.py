0c2a6b78352e8a9102e981ddd4af81e0e0a0c457
Anastasiia Karavaieva <anastasia.karavaeva1961@gmail.com>
2023-03-29 22:24:20 +0300

    Added code to HW_4

--- AnastasiiaKaravaieva/HW_4/1.py
+++ AnastasiiaKaravaieva/HW_4/1.py
@@ -1,2 +1,9 @@
-# in process
-# still solve
+
+c = int(input("Write your temperature in Celsius:"))
+
+if c < -273.15:
+    print("Error. You can't convert temperature below absolute zero(-273.15 C")
+else:
+    f = (c * 9/5) + 32
+    print(f"{c} C is equivalent to {f} F ")
+
