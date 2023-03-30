8134b08161426f3f1aa5237161be6ab39a683e01
Yan Zhylavy <yanzhylavy@gmail.com>
2023-03-30 15:13:49 +0300

    Added Practical tasks 1, 2

--- YanZhylavy/HW5/Practical task 5.1.py
+++ YanZhylavy/HW5/Practical task 5.1.py
@@ -1,11 +1,4 @@
-elements = int(input('''Please, tell how many elements
-from Fibonacci`s sequence you`d like to see: '''))
-  
-n1 = 0
-n2 = 1
-
-for i in range(elements):
-    print(n1)
-    n3 = n1 + n2
-    n1 = n2
-    n2 = n3
+list = [34,5,26,78,45,74,8096]
+for i in list:
+    i = float(i)
+    print(i)
