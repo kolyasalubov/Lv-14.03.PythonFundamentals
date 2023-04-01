ed0b68bd32e0b2eb7b5cbe2d90a329b34d26fa9f
Victor Dyminskyi <victor.dyminskyi@gmail.com>
2023-04-01 16:15:48 +0300

    added done HW5

---
+++ HW5/HW5.py
@@ -0,0 +1,52 @@
+# 1 TASK---------------------------------------------
+
+li = [1, 2, 3, 4, 5]
+
+for i in li:
+    chan = float(i)
+    print(chan, type(chan))
+
+# 2 TASK---------------------------------------------
+
+
+#
+#
+#
+# li1 = [0, 1]
+# for i in li1:
+#     if i < 10:
+#         fib = li1[i] + li1[i+1]
+#         add = li1.append(fib)
+#         print(i, i+1, li1[i], li1[i + 1], li1)
+#     else:
+#         print("done")
+
+# print(li1)
+
+
+n = int(input("Write down your number: "))
+
+li1 = [0, 1]
+fib = 0
+for i in range(n):
+    if fib < n:
+        fib = li1[i] + li1[i+1]
+        addd = li1.append(fib)
+        # print(i)
+    else:
+        break
+print(li1[:-1])
+
+# 3 TASK---------------------------------------------
+
+factorial = int(input("enter your factorial: "))
+list = [0, 1]
+
+if factorial >= 0:
+    for i in range(factorial):
+        fact = list[-1] * (i+1)
+        # print(fact)
+        list.append(fact)
+    print(f"your result is {list[-1]}")
+else:
+    print("error, your number {} cannot be used".format(factorial))
