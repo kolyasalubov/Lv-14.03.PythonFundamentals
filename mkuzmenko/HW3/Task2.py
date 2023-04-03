60a344802fdd1184238e5780ae96dc74cd2a2137
Marina Kuzmenko <marina.kuzmenko.01@gmail.com>
2023-04-03 22:44:56 +0200

    added homework 4

--- mkuzmenko/HW3/Task2.py
+++ mkuzmenko/HW3/Task2.py
@@ -1,4 +1,5 @@
-number = int(input("Enter a four-digit number: "))
+number = input("Enter a four-digit number: ")
 
-print("Number in reverse order:", str(number)[::-1])
-print("Sorted numbers:", sorted(str(number)))
+# print("Number in reverse order:", str(number)[::-1])
+# print("Sorted numbers:", sorted(str(number)))
+print(f'Sorted list': ("".join(sorted(number))))
