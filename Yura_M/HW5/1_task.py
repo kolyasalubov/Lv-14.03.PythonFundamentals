85ff4b67b753b8f1eb54919e98353c19b1b7f1ab
Yura maliutin <joker199378@gmail.com>
2023-04-04 01:30:27 +0300

    added HW 5 Task 2

--- Yura_M/HW5/1_task.py
+++ Yura_M/HW5/1_task.py
@@ -4,3 +4,7 @@ for value in integer:
     b = float(value)
     floting_type.append(b)
 print("float element", floting_type)
+
+
+# integer = [15, 25, 78, 94, 31]
+# floting_type = [float(value) for value in integer]
