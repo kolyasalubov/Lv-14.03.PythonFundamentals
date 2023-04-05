a3813fa5dea7d233963516231a6087175510cd32
Yan Zhylavy <yanzhylavy@gmail.com>
2023-04-06 00:22:27 +0300

    Corrected practical task 5.2 according to the mentors recommendation

--- YanZhylavy/HW5/Practical task 5.2.py
+++ YanZhylavy/HW5/Practical task 5.2.py
@@ -1,11 +1,13 @@
-elements = int(input('''Please, tell how many elements
-from Fibonacci`s sequence you`d like to see: '''))
-  
-n1 = 0
-n2 = 1
-
-for i in range(elements):
-    print(n1)
-    n3 = n1 + n2
-    n1 = n2
-    n2 = n3
+try:
+    elements = int(input('''Please, tell how many elements
+    from Fibonacci`s sequence you`d like to see: '''))  
+    fibonacci_numbers = [0, 1]
+    if elements == 0 or elements == 1:
+        print(0) 
+    else:  
+        for i in range(2, elements):
+            fibonacci_numbers.append(fibonacci_numbers[i-2]+fibonacci_numbers[i-1])
+        print(fibonacci_numbers)   
+except:
+    print('Are you okay?')
+    
