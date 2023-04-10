8d7c9b689cea886b06744036b7c514a709749eea
Anastasiia Karavaieva <anastasia.karavaeva1961@gmail.com>
2023-04-10 23:38:45 +0300

    Added Task 2 to HW_7

---
+++ AnastasiiaKaravaieva/HW_7/2.py
@@ -0,0 +1,33 @@
+import math
+
+
+def triangle_area(h, a):
+    s = 1 / 2 * h * a
+    return int(s)
+
+
+def rectangle_area(a, b):
+    s = a * b
+    return s
+
+
+def circle_area(r):
+    s = r*r*math.pi
+    return round(s, 2)
+
+
+area = input("What kind of area do you want to find?")
+
+if area == "triangle":
+    h_t = int(input("Write the height of triangle:"))
+    a_t = int(input("Write the side of triangle:"))
+    print(triangle_area(h_t, a_t))
+elif area == "rectangle":
+    a_r = int(input("Write the first side of rectangle:"))
+    b_r = int(input("Write the second side of rectangle:"))
+    print(rectangle_area(a_r, b_r))
+elif area == "circle":
+    r_c = int(input("Write the r of the circle:"))
+    print(circle_area(r_c))
+else:
+    print("I don't know how to find the area of this figure")
