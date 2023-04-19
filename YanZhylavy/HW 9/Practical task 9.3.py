8072494e38c181c996f3db6dc036b52796a12ead
Yan Zhylavy <yanzhylavy@gmail.com>
2023-04-19 12:45:55 +0300

    Improved Practical task 9.3

--- YanZhylavy/HW 9/Practical task 9.3.py
+++ YanZhylavy/HW 9/Practical task 9.3.py
@@ -1,15 +1,26 @@
 import tkinter as tk
-HEIGHT = 350
-WIDTH = 450
+HEIGHT = 400
+WIDTH = 550
 def get_weather():
-  
-  from pyowm import OWM
-  owm = OWM('ef2206ff5da67de63306d0b143e20872')
-  mgr = owm.weather_manager()
-  input_city = entry_field.get()
-  observation = mgr.weather_at_place(input_city)
-  w = observation.weather
-  label['text'] = w.detailed_status
+      label['text'] = weather_response()
+
+
+
+def weather_response(): 
+      from pyowm import OWM
+      owm = OWM('ef2206ff5da67de63306d0b143e20872')
+      mgr = owm.weather_manager()
+      input_city = entry_field.get()
+      observation = mgr.weather_at_place(input_city)
+      w = observation.weather 
+      temperature = w.temperature('celsius')
+      weather_info = f'Generaly, we have {w.detailed_status} here.\n' \
+                     f'Wind speed is {w.wind()["speed"]} meters per second.\n' \
+                     f'Current air temperature is {int(temperature["temp"])} °С.\n' \
+                     f'Air humidity is {w.humidity} %'  
+                               
+      
+      return weather_info                
 
   
 root = tk.Tk()
@@ -36,11 +47,11 @@ button.place(relx=0.7, rely=0, relwidth=0.3, relheight=1)
 
 
 
-lower_frame = tk.Frame(root, bg='gold', bd=10)
+lower_frame = tk.Frame(root, bg='deep sky blue', bd=10)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
 
-label = tk.Label(lower_frame, font=('Courier', 14))
+label = tk.Label(lower_frame, font=('Courier', 11), justify='center')
 label.place(relx=0, rely=0, relwidth=1, relheight=1)
 
 
