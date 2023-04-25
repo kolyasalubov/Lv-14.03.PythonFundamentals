174a4e38c89ac44adf0d9342fbdcf993a0a5a30f
Alex Sychov <alexandersychev2005@gmail.com>
2023-04-25 19:29:50 +0300

    4

--- osychov/Project/Web app/app.py
+++ osychov/Project/Web app/app.py
@@ -1,7 +1,7 @@
+import os
 import requests
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
-import os
+from flask_sqlalchemy import SQLAlchemy 
 
 
 app = Flask(__name__)
@@ -11,37 +11,33 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,"wea
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'thisisasecret'
 
-
-
-
 db = SQLAlchemy(app)
-
 
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
 def get_weather_data(city):
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=ef2206ff5da67de63306d0b143e20872&units=metric"
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={ city }&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
     r = requests.get(url).json()
-    return r 
+    return r
 
 @app.route('/')
 def index_get():
     cities = City.query.all()
+
     weather_data = []
 
     for city in cities:
-        r= get_weather_data(city.name)
+
+        r = get_weather_data(city.name)
+        print(r)
+
         weather = {
             'city' : city.name,
             'temperature' : r['main']['temp'],
-            'feel_like' : r["main"]["feels_like"],
             'description' : r['weather'][0]['description'],
             'icon' : r['weather'][0]['icon'],
-            'wind' : r["wind"]["speed"],
-            'humidity' : r["main"]["humidity"],
-            'pressure' : r["main"]["pressure"],
         }
 
         weather_data.append(weather)
@@ -51,30 +47,41 @@ def index_get():
 
 @app.route('/', methods=['POST'])
 def index_post():
-    err_mess = ''
+    err_msg = ''
     new_city = request.form.get('city')
-    if new_city:
-        existing_city = City.query.filter_by(name = new_city).first()
         
+    if new_city:
+        existing_city = City.query.filter_by(name=new_city).first()
+
         if not existing_city:
             new_city_data = get_weather_data(new_city)
+
             if new_city_data['cod'] == 200:
                 new_city_obj = City(name=new_city)
 
                 db.session.add(new_city_obj)
                 db.session.commit()
             else:
-                err_mess = 'City does not exists in the world!'
+                err_msg = 'City does not exist in the world!'
         else:
-            err_mess = 'City already exists in database!'
-            
-    if err_mess:
-        flash(err_mess, 'error')
+            err_msg = 'City already exists in the database!'
+
+    if err_msg:
+        flash(err_msg, 'error')
     else:
         flash('City added succesfully!')
-        
+
     return redirect(url_for('index_get'))
+
+@app.route('/delete/<name>')
+def delete_city(name):
+    city = City.query.filter_by(name=name).first()
+    db.session.delete(city)
+    db.session.commit()
+
+    flash(f'Successfully deleted { city.name }', 'success')
+    return redirect(url_for('index_get'))
+
 
 if __name__ == '__main__':
     app.run(debug = True, port=5000)
-    
