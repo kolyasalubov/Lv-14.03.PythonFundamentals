6c96191d86b5c0264dac3a7d0f215c922b0cad4d
Alex Sychov <alexandersychev2005@gmail.com>
2023-04-25 03:58:09 +0300

    Added feature: fixed repeating cities in web flask weather application

--- osychov/Project/Web app/app.py
+++ osychov/Project/Web app/app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -8,6 +8,10 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,"weather.db")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'thisisasecret'
+
+
 
 
 db = SQLAlchemy(app)
@@ -17,27 +21,18 @@ class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        new_city = request.form.get('city')
-        
-        if new_city:
-            new_city_obj = City(name=new_city)
+def get_weather_data(city):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=ef2206ff5da67de63306d0b143e20872&units=metric"
+    r = requests.get(url).json()
+    return r 
 
-            db.session.add(new_city_obj)
-            db.session.commit()
-
+@app.route('/')
+def index_get():
     cities = City.query.all()
-
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=ef2206ff5da67de63306d0b143e20872&units=metric"
-
     weather_data = []
 
     for city in cities:
-
-        r = requests.get(url.format(city.name)).json()
-
+        r= get_weather_data(city.name)
         weather = {
             'city' : city.name,
             'temperature' : r['main']['temp'],
@@ -54,6 +49,31 @@ def index():
 
     return render_template('weather.html', weather_data=weather_data)
 
+@app.route('/', methods=['POST'])
+def index_post():
+    err_mess = ''
+    new_city = request.form.get('city')
+    if new_city:
+        existing_city = City.query.filter_by(name = new_city).first()
+        
+        if not existing_city:
+            new_city_data = get_weather_data(new_city)
+            if new_city_data['cod'] == 200:
+                new_city_obj = City(name=new_city)
+
+                db.session.add(new_city_obj)
+                db.session.commit()
+            else:
+                err_mess = 'City does not exists in the world!'
+        else:
+            err_mess = 'City already exists in database!'
+            
+    if err_mess:
+        flash(err_mess, 'error')
+    else:
+        flash('City added succesfully!')
+        
+    return redirect(url_for('index_get'))
 
 if __name__ == '__main__':
     app.run(debug = True, port=5000)
